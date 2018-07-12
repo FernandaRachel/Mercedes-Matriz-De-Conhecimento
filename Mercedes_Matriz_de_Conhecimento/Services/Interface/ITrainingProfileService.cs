1ea23ab06bc6845d2148aa3907476f1b3624c1f8
FernandaRachel <fefedel9@hotmail.com>
2018-07-12 09:09:14 -0300

    TrainingProfile Service - created

---
+++ Mercedes_Matriz_de_Conhecimento/Services/Interface/ITrainingProfileService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercedes_Matriz_de_Conhecimento.Services.Interface
+{
+    interface ITrainingProfileService
+    {
+
+        IEnumerable<tblPerfilTreinamento> GetTrainingProfiles();
+
+        tblPerfilTreinamento GetTrainingProfileById(int id);
+
+        tblPerfilTreinamento CreateTrainingProfile(tblPerfilTreinamento TrainingProfile);
+
+        tblPerfilTreinamento UpdateTrainingProfile(tblPerfilTreinamento TrainingProfileProfile);
+
+        tblPerfilTreinamento DeleteTrainingProfile(int id);
+
+        bool checkIfTrainingProfileAlreadyExits(tblPerfilTreinamento TrainingProfile);
+
+
+    }
+}
---
+++ Mercedes_Matriz_de_Conhecimento/Services/TrainingProfileService.cs
@@ -0,0 +1,106 @@
+﻿using Mercedes_Matriz_de_Conhecimento.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace Mercedes_Matriz_de_Conhecimento.Services
+{
+    public class TrainingProfileService : ITrainingProfileService
+    {
+
+        public DbConnection _db = new DbConnection();
+
+
+
+        public tblPerfilTreinamento GetTrainingProfileById(int id)
+        {
+            tblPerfilTreinamento trainingProfile;
+
+            var query = from f in _db.tblPerfilTreinamento
+                        where f.IdPerfilTreinamento == id
+                        orderby f.Nome
+                        select f;
+
+            trainingProfile = query.FirstOrDefault();
+
+            return trainingProfile;
+        }
+
+        public IEnumerable<tblPerfilTreinamento> GetTrainingProfiles()
+        {
+            IEnumerable<tblPerfilTreinamento> trainingProfile;
+
+
+
+            var query = from f in _db.tblPerfilTreinamento
+                        orderby f.Nome
+                        select f;
+
+            trainingProfile = query.AsEnumerable();
+
+            return trainingProfile;
+        }
+
+
+        public tblPerfilTreinamento CreateTrainingProfile(tblPerfilTreinamento TrainingProfile)
+        {
+            _db.tblPerfilTreinamento.Add(TrainingProfile);
+
+            _db.SaveChanges();
+
+
+            return TrainingProfile;
+        }
+
+        public tblPerfilTreinamento DeleteTrainingProfile(int id)
+        {
+            tblPerfilTreinamento TrainingProfile;
+
+            var query = from f in _db.tblPerfilTreinamento
+                        where f.IdPerfilTreinamento == id
+                        orderby f.Nome
+                        select f;
+
+            TrainingProfile = query.FirstOrDefault();
+
+            _db.tblPerfilTreinamento.Remove(TrainingProfile);
+            _db.SaveChanges();
+
+            return TrainingProfile;
+        }
+
+        
+        public tblPerfilTreinamento UpdateTrainingProfile(tblPerfilTreinamento TrainingProfile)
+        {
+            var trainingProfileToUpdate = _db.tblPerfilTreinamento.Find(TrainingProfile.IdPerfilTreinamento);
+            trainingProfileToUpdate.Nome = TrainingProfile.Nome;
+            trainingProfileToUpdate.tblTipoTreinamento= TrainingProfile.tblTipoTreinamento;
+            trainingProfileToUpdate.tblPerfilTreinamentoxPerfilItem = TrainingProfile.tblPerfilTreinamentoxPerfilItem;
+           
+
+
+            _db.Entry(trainingProfileToUpdate).State = EntityState.Modified;
+            _db.SaveChanges();
+
+
+            return trainingProfileToUpdate;
+        }
+
+
+        public bool checkIfTrainingProfileAlreadyExits(tblPerfilTreinamento TrainingProfile)
+        {
+            var query = from f in _db.tblPerfilTreinamento
+                        where f.Nome == TrainingProfile.Nome
+                        orderby f.Nome
+                        select f;
+
+            if (query.Count() == 1 && query.FirstOrDefault().IdPerfilTreinamento != TrainingProfile.IdPerfilTreinamento)
+                return true;
+
+            return false;
+        }
+    }
+}
