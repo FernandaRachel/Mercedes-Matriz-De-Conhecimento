82029e0ae2deb2aeeaaab024085e72d455624b64
FernandaRachel <fefedel9@hotmail.com>
2018-07-19 13:46:05 -0300

    TrainingStatus Service/Controller - created

---
+++ Mercedes_Matriz_de_Conhecimento/Services/Interface/ITrainingStatusService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercedes_Matriz_de_Conhecimento.Services.Interface
+{
+    interface ITrainingStatusService
+    {
+
+        IEnumerable<tblTreinamentoStatus> GetTrainingStatuss();
+
+        tblTreinamentoStatus GetTrainingStatusById(int id);
+
+        tblTreinamentoStatus CreateTrainingStatus(tblTreinamentoStatus TrainingStatusStatus);
+
+        tblTreinamentoStatus UpdateTrainingStatus(tblTreinamentoStatus TrainingStatusStatus);
+
+        tblTreinamentoStatus DeleteTrainingStatus(int id);
+
+        bool checkIfTrainingStatusAlreadyExits(tblTreinamentoStatus TrainingStatusStatus);
+
+
+    }
+}
---
+++ Mercedes_Matriz_de_Conhecimento/Services/TrainingStatusService.cs
@@ -0,0 +1,102 @@
+﻿using Mercedes_Matriz_de_Conhecimento.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace Mercedes_Matriz_de_Conhecimento.Services
+{
+    public class TrainingStatusService : ITrainingStatusService
+    {
+
+        public DbConnection _db = new DbConnection();
+
+
+        public tblTreinamentoStatus GetTrainingStatusById(int id)
+        {
+            tblTreinamentoStatus TrainingStatus;
+
+            var query = from f in _db.tblTreinamentoStatus
+                        where f.idStatusTreinamento == id
+                        orderby f.Denominacao
+                        select f;
+
+            TrainingStatus = query.FirstOrDefault();
+
+            return TrainingStatus;
+        }
+
+        public IEnumerable<tblTreinamentoStatus> GetTrainingStatuss()
+        {
+            IEnumerable<tblTreinamentoStatus> TrainingStatus;
+
+
+
+            var query = from f in _db.tblTreinamentoStatus
+                        orderby f.Denominacao
+                        select f;
+
+            TrainingStatus = query.AsEnumerable();
+
+            return TrainingStatus;
+        }
+
+
+        public tblTreinamentoStatus CreateTrainingStatus(tblTreinamentoStatus TrainingStatus)
+        {
+            _db.tblTreinamentoStatus.Add(TrainingStatus);
+
+            _db.SaveChanges();
+
+
+            return TrainingStatus;
+        }
+
+        public tblTreinamentoStatus DeleteTrainingStatus(int id)
+        {
+            tblTreinamentoStatus TrainingStatus;
+
+            var query = from f in _db.tblTreinamentoStatus
+                        where f.idStatusTreinamento == id
+                        orderby f.Denominacao
+                        select f;
+
+            TrainingStatus = query.FirstOrDefault();
+
+            _db.tblTreinamentoStatus.Remove(TrainingStatus);
+            _db.SaveChanges();
+
+            return TrainingStatus;
+        }
+
+        
+        public tblTreinamentoStatus UpdateTrainingStatus(tblTreinamentoStatus TrainingStatus)
+        {
+            var TrainingStatusToUpdate = _db.tblTreinamentoStatus.Find(TrainingStatus.idStatusTreinamento);
+            TrainingStatusToUpdate.Denominacao = TrainingStatus.Denominacao;
+            TrainingStatusToUpdate.Cor = TrainingStatus.Cor;
+
+            _db.Entry(TrainingStatusToUpdate).State = EntityState.Modified;
+            _db.SaveChanges();
+
+
+            return TrainingStatusToUpdate;
+        }
+
+
+        public bool checkIfTrainingStatusAlreadyExits(tblTreinamentoStatus TrainingStatus)
+        {
+            var query = from f in _db.tblTreinamentoStatus
+                        where f.Denominacao == TrainingStatus.Denominacao
+                        orderby f.Denominacao
+                        select f;
+
+            if (query.Count() == 1 && query.FirstOrDefault().idStatusTreinamento != TrainingStatus.idStatusTreinamento)
+                return true;
+
+            return false;
+        }
+    }
+}
