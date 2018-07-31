71685d0a3ce03b92e60e774f0dcbb3adb37d5c65
FernandaRachel <fefedel9@hotmail.com>
2018-07-31 15:12:08 -0300

    layout being standardized to look like Mercedes standard

--- Mercedes_Matriz_de_Conhecimento/App_Start/BundleConfig.cs
+++ Mercedes_Matriz_de_Conhecimento/App_Start/BundleConfig.cs
@@ -28,6 +28,7 @@ namespace Mercedes_Matriz_de_Conhecimento
                       "~/Content/bootstrap.css",
                       "~/Content/bootstrap-select.css",
                       "~/Content/mercedes.css",
+                      "~/Content/mbbras.css",
                       "~/Content/site.css"));
         }
     }
